cdc2724cf91e5206e057db9134d7c312dae80f78
M'mah Zombo <www.mariamzombo@gmail.com>
2023-07-27 16:39:53 +0000

    changedm the entire code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,24 +6,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    # Initialize the triangle with the first row
     triangle = [[1]]
-
     for i in range(1, n):
-        prev_row = triangle[-1]
-        new_row = [1]
-
-        for j in range(1, len(prev_row)):
-            new_row.append(prev_row[j - 1] + prev_row[j])
-
-        new_row.append(1)
-        triangle.append(new_row)
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
-
-
-# Test the function
-n = 5
-result = pascal_triangle(n)
-for row in result:
-    print(row)
