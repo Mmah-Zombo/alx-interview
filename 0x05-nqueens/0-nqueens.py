58bd94d11021b45730f295ea15f19c868a9f1069
M'mah Zombo <www.mariamzombo@gmail.com>
2023-08-31 18:24:02 +0100

    test

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""the nqueen solution"""
-
 
 import sys
 
 def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
+    # Check if there's a queen in the same column
     for i in range(row):
         if board[i][col] == 1:
             return False
-
+    
     # Check upper left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
-
+    
     # Check upper right diagonal
     for i, j in zip(range(row, -1, -1), range(col, N)):
         if board[i][j] == 1:
             return False
-
+    
     return True
 
 def solve_nqueens_util(board, row, N):
@@ -27,10 +25,11 @@ def solve_nqueens_util(board, row, N):
         for i in range(N):
             for j in range(N):
                 if board[i][j] == 1:
-                    print(f'[{i}, {j}]', end=" ")
+                    print(f'[{i}, {j}]', end="")
+            print()
         print()
         return
-
+    
     for col in range(N):
         if is_safe(board, row, col, N):
             board[row][col] = 1
@@ -41,17 +40,19 @@ def solve_nqueens(N):
     if not N.isdigit():
         print("N must be a number")
         sys.exit(1)
+    
     N = int(N)
+    
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    board = [[0] * N for _ in range(N)]
+    
+    board = [[0 for _ in range(N)] for _ in range(N)]
     solve_nqueens_util(board, 0, N)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
+    
     solve_nqueens(sys.argv[1])
-
