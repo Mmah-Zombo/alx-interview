b7e8084828eb2958e0d1a0dedc1125bf8fc9bbb0
M'mah Zombo <www.mariamzombo@gmail.com>
2023-09-14 14:04:42 +0000

    rotates the matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains a fucntion that rotatesa 2D matrix by 90 degrees"""
+from typing import List
+
+
+def rotate_2d_matrix(matrix: List[List]) -> None:
+    """a function that rotates a 2D matrix"""
+
+    matrix.reverse()
+    row1 = [each for row in matrix for each in row if row.index(each) == 0]
+    row2 = [each for row in matrix for each in row if row.index(each) == 1]
+    row3 = [each for row in matrix for each in row if row.index(each) == 2]
+    matrix.clear()
+    matrix.append(row1)
+    matrix.append(row2)
+    matrix.append(row3)
